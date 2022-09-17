dd9e56159685d251e31d74b9b0d819d5e1c4aaab
amartin755 <netnag@mailbox.org>
2022-09-17 13:34:57 +0200

    added licence header

--- src/application.hpp
+++ src/application.hpp
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-only
 /*
- * TCPPUMP <https://github.com/amartin755/tcppump>
- * Copyright (C) 2012-2021 Andreas Martin (netnag@mailbox.org)
+ * NET-BABBLER <https://github.com/amartin755/tcppump>
+ * Copyright (C) 2022 Andreas Martin (netnag@mailbox.org)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/client.cpp
+++ src/client.cpp
@@ -1,3 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0-only
+/*
+ * NET-BABBLER <https://github.com/amartin755/tcppump>
+ * Copyright (C) 2022 Andreas Martin (netnag@mailbox.org)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netdb.h>
